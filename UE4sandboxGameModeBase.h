88e1171bb52e3b25a8a5805fc917b3615b792085
naritta <narittan@gmail.com>
2021-08-19 23:04:23 +0900

    Add project for using usf

---
+++ UE4sandboxGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4sandboxGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4SANDBOX_API AUE4sandboxGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
